3d0ae1f76cc0d54586ce524279dc5b458f5a9c5b
zx9w <ilmu@rishi.is>
2019-07-02 18:20:56 +0000

    Total nerdsnipe! I'm looking into unboxed ints...

--- Collatz.hs
+++ Collatz.hs
@@ -1,19 +1,26 @@
-module Collatz
-where
-
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE BangPatterns #-}
 import Data.List
+import Data.Ord
 
 collatz :: Int -> Int
 collatz x | even x    = x `div` 2
           | otherwise = 3*x+1
 
 collSeq :: Int -> [Int]
-collSeq y = go y []
+collSeq y = go y [] -- No space leak since tail recursive
   where
     go :: Int -> [Int] -> [Int]
-    go 1 xs = (1:xs)
-    go x xs = go (collatz x) (x:xs)
-    go _ _  = error "the collatz conjecture is untrue!"
+    go 1 !xs = (1:xs)
+    go x !xs = go (collatz x) (x:xs)
 
-findSecondLongest :: [Int] -> Int
-findSecondLongest xs = length $ head $ tail $ reverse $ sortOn length (collSeq <$> xs)
+secondSeq :: Int -> [Int]
+secondSeq z = head $ tail $ sortBy (\x y->(comparing Down) (length x) (length y)) (collSeq <$> [1..z])
+
+tenmillion = 10000000 :: Int -- avoid typo
+million    = 1000000 :: Int
+
+main :: IO()
+main = do
+  print (length $ secondSeq million)
+  print (sum $ secondSeq million)
